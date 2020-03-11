bf92ffefc98df547831ec4dbbd724c48661198c2
mapleeit <522856232@qq.com>
2020-03-11 17:09:00 +0800

    refactor: 增加代码健壮性和日志

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const URL = require("url");
 const moment = require('moment');
 const { signature } = require("./sig");
 const { encode } = require("./encrypt");
-const ip = require('ip');
+const ip = require("ip");
 
 const apiDomain = "openapi.tswjs.org";
 const url = `https://${apiDomain}/v1/log/report`;
@@ -35,63 +35,116 @@ class OpenPlatformPlugin {
     this.reportList = false;
     this.proxyInfo = {};
 
+    // 默认给一个返回 undefined 的同步函数
     this.getUid = config.getUid || (() => {});
-    this.getProxyInfo = config.getProxyInfo || (() => {});
-    this.getReportList = config.getReportList || (() => {});
+    // 默认给一个返回 {} 的同步函数
+    this.getProxyInfo = config.getProxyInfo || (() => ({}));
+    // 默认给一个返回 false 的同步函数
+    this.getReportList = config.getReportList || (() => false);
   }
 
   /**
    * 插件初始化
    */
   async init(eventBus, config) {
+    this.log("插件开始加载...");
+
+    this.log("获取 reportList 中...");
+    this.reportList = await this.getReportList();
+    this.log("获取 proxyInfo 中...");
+    this.proxyInfo = await this.getProxyInfo();
+
+    this.log("断言参数类型是否符合预期...");
+    this.assertParams();
+
+    this.extendProxyInfo(this.proxyInfo);
+    this.log("上传 proxyInfo 中...");
+    await this.reportProxyEnv();
+
+    /**
+     * 请求开始时，提取 uid
+     */
     eventBus.on("REQUEST_START", (payload) => {
       const { req, context } = payload;
-      const proxyInfo = this.proxyInfo;
     
       context.uid = this.getUid(req);
     
-      for( let proxyIp of Object.keys(proxyInfo) ){
-        if(proxyInfo[proxyIp].alphaList.indexOf(context.uid) !== -1){
+      for (const proxyIp of Object.keys(this.proxyInfo)) {
+        if (this.proxyInfo[proxyIp].alphaList.indexOf(context.uid) !== -1) {
           context.proxyIp = proxyIp;
-          context.proxyPort = proxyInfo[proxyIp].port || "80";
+          context.proxyPort = this.proxyInfo[proxyIp].port || "80";
           break;
         }
       }
     })
     
+    /**
+     * 响应结束时，进行日志上报
+     */
     eventBus.on("RESPONSE_FINISH", (payload) => {
       const { req, res, context } = payload;
     
-      if(context.uid === "") return console.log("Not set uid");
-      if(this.reportList === false) return console.log("Not open report");
-      if(this.reportList === true ||
-        (this.reportList instanceof Array && this.reportList.indexOf(context.uid) !== -1)) 
+      if (!context.uid) {
+        this.log(`请求结束日志不上报，因为 uid 为空或者 undefined`);
+        return;
+      }
+
+      if (this.reportList === false) {
+        this.log(`请求结束日志不上报，因为 reportList 为 false`);
+        return;
+      }
+
+      if (this.reportList === true) {
+        this.log(`请求结束日志上报，因为 reportList 为 true`);
         return this.reportLog(req, res, context);
-    
-      return console.log("Current user not in reportList");
+      }
+
+      if (Array.isArray(this.reportList)
+        && this.reportList.indexOf(context.uid) !== -1) {
+        this.log(`请求结束日志上报，因为用户 ${context.uid} 在 reportList 中`);
+        return this.reportLog(req, res, context);
+      }
     })
 
-    this.reportList = await this.getReportList();
-    this.updateProxyEnvByFn(await this.getProxyInfo());
+    await this.updateProxyEnvByCloud();
 
     // 周期性更新代理名单
     setInterval(()=>{
+      this.log("从开放平台同步测试号码（频率为 1min）...");
       this.updateProxyEnvByCloud();
     }, 60000);
+
+    this.log("插件加载完毕")
+  }
+
+  assertParams() {
+    if (!this.appid) {
+      throw new Error(`参数 appid 不能为空`);
+    }
+
+    if (!this.appkey) {
+      throw new Error(`参数 appkey 不能为空`)
+    }
+
+    if (typeof this.reportList !== "boolean"
+      && !Array.isArray(this.reportList)) {
+      throw new Error(`参数 getReportList 函数必须返回布尔值或者字符串数组`)
+    }
+
+    // TODO: 这里应该使用 joi 来进行更加详细的断言
+    if (typeof this.proxyInfo !== "object") {
+      throw new Error(`参数 getProxyInfo 函数必须返回一个对象`)
+    }
   }
 
   /**
    * 上报代理环境
    */
-  reportProxyEnv() {
-    console.log('reportProxyEnv')
-    const proxyInfos = this.proxyInfo;
-    if(!proxyInfos) return console.log('Not set proxy env');
-  
+  async reportProxyEnv() {
     const intranetIp = ip.address();
-    const proxyInfo = proxyInfos[intranetIp];
+    const proxyInfo = this.proxyInfo[intranetIp];
   
-    if(!proxyInfo) return console.log('Not set current machine as proxy env');
+    if (!proxyInfo) return this.log(`不允许通过代理访问本机器，不上报开放平台`);
   
     const logText = `${intranetIp}:${proxyInfo.port ? proxyInfo.port : '80'}`;
     let logJson = Object.assign({
@@ -128,21 +181,24 @@ class OpenPlatformPlugin {
       appkey: this.appkey
     });
   
-    axios.post(url, data, {
+    await axios.post(url, data, {
       responseType: "json"
     }).then(d => {
-      console.log(d.data);
+      if (d.data.code !== 0) throw new Error(d.data.message);
     }).catch(e => {
-      console.error(e);
+      this.log(`上报代理环境失败: ${e.message}`);
     })
   }
 
-  updateProxyEnvByCloud() {
-    console.log('updateProxyEnvByCloud')
+  /**
+   * 从开放平台同步代理名单
+   */
+  async updateProxyEnvByCloud() {
     const data = {
       appid: this.appid,
       now: Date.now()
     };
+
     data.sig = signature({
       pathname: URL.parse(h5testSyncUrl).pathname,
       method: 'POST',
@@ -150,49 +206,44 @@ class OpenPlatformPlugin {
       appkey: this.appkey
     });
 
-    axios.post(h5testSyncUrl, data, {
+    await axios.post(h5testSyncUrl, data, {
       responseType: "json"
     }).then(d => {
-      console.log(d.data);
-      let remoteProxyInfo = d.data.data;
-      if(JSON.stringify(remoteProxyInfo) === "{}") return;
-      const proxyInfo = this.proxyInfo;
-      for(let uid of Object.keys(remoteProxyInfo)){
-        const ip = remoteProxyInfo[uid].split(":")[0];
-        if(!proxyInfo[ip]){
-          proxyInfo[ip] = { alphaList: [uid] };
-        }else if(proxyInfo[ip] && proxyInfo[ip].alphaList.indexOf(uid) === -1) {
-          proxyInfo[ip].alphaList.push(uid);
-        }else {
-          return;
-        }
+      if (d.data.code !== 0) throw new Error(d.data.message);
+
+      const remoteProxyInfo = d.data.data;
+      for(const uid of Object.keys(remoteProxyInfo)){
+        const [ip] = remoteProxyInfo[uid].split(":");
+
+        const data = {};
+        data[ip] = { alphaList: [uid] };
+        this.extendProxyInfo(data);
       }
     }).catch(e => {
-      console.error(e);
+      this.log(`代理名单更新失败: ${e.message}`);
     })
   }
 
-  updateProxyEnvByFn(extendProxyInfo) {
-    console.log('updateProxyEnvByFn');
-    if(JSON.stringify(extendProxyInfo) === "{}") return;
-    const proxyInfo = this.proxyInfo;
+  extendProxyInfo(extendProxyInfo) {
+    for (const ip of Object.keys(extendProxyInfo)){
+      if (this.proxyInfo[ip] && this.proxyInfo[ip].alphaList){
+        const newAlphaList = Array.from(
+          new Set(
+            extendProxyInfo[ip].alphaList.concat(this.proxyInfo[ip].alphaList)
+          )
+        );
 
-    for(let ip of Object.keys(extendProxyInfo)){
-      if(proxyInfo[ip] && proxyInfo[ip].alphaList){
-        const newAlphaList = Array.from(new Set( extendProxyInfo[ip].alphaList.concat(proxyInfo[ip].alphaList) ));
-        proxyInfo[ip] = Object.assign(proxyInfo[ip], extendProxyInfo[ip]);
-        proxyInfo[ip].alphaList = newAlphaList;
-      }else if(proxyInfo[ip] && !proxyInfo[ip].alphaList) {
-        proxyInfo[ip] = Object.assign(proxyInfo[ip], extendProxyInfo[ip]);
-      }else {
-        proxyInfo[ip] = extendProxyInfo[ip];
+        this.proxyInfo[ip] = Object.assign(this.proxyInfo[ip], extendProxyInfo[ip]);
+        this.proxyInfo[ip].alphaList = newAlphaList;
+      } else if(this.proxyInfo[ip] && !this.proxyInfo[ip].alphaList) {
+        this.proxyInfo[ip] = Object.assign(this.proxyInfo[ip], extendProxyInfo[ip]);
+      } else {
+        this.proxyInfo[ip] = extendProxyInfo[ip];
       }
     }
-
-    this.reportProxyEnv();
   }
 
-  reportLog(req, res, context) {
+  async reportLog(req, res, context) {
     const { captureRequests, currentRequest } = context;
 
     captureRequests.map(item => {
@@ -251,13 +302,19 @@ class OpenPlatformPlugin {
       appkey: this.appkey
     });
   
-    axios.post(url, data, {
+    await axios.post(url, data, {
       responseType: "json"
     }).then(d => {
-      console.log(d.data);
+      if (d.data.code !== 0) throw new Error(d.data.message);
+
+      this.log(`上报日志成功`);
     }).catch(e => {
-      console.error(e);
+      this.log(`上报日志失败: ${e.message}`);
     })
+  }
+
+  log(string) {
+    console.debug(`[${this.name}]: ${string}`)
   }
 }
 
